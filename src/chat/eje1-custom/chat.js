1975d02d2a10b56088ebf46457ba793fb34becc0
simonll4 <sllamosas806@alumnos.iua.edu.ar>
2024-05-15 11:47:30 -0300

    conexion entre 2 user basica

---
+++ src/chat/eje1-custom/chat.js
@@ -0,0 +1,110 @@
+import { io } from 'https://cdn.socket.io/4.3.2/socket.io.esm.min.js'
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Este código se ejecutará después de que el DOM esté listo
+
+  const idArticle = localStorage.getItem('articleId');
+  localStorage.removeItem('articleId');
+  const token = localStorage.getItem('token');
+  console.log('hola:', token);
+
+
+  const form = document.getElementById('formMsg');
+  const input = document.getElementById('inputMsg');
+  const messages = document.getElementById('messages');
+
+  // establezco conexión con el servidor de socket
+  // paso el token como query
+  const socket = io('http://localhost:5050', {
+    query: {
+      token: token
+    }
+  });
+
+  (() => {
+    socket.emit('chat history', idArticle);
+    console.log('id articulo: ', idArticle);
+  })();
+
+    let sesionId
+   // obtengo id de la sesión
+   socket.on('send sesionId', (idSesion) => {
+    sesionId = idSesion;
+    console.log('idSesion:', sesionId);
+  });
+
+ 
+
+  // si hay un historial de mensajes, los muestro en el chat
+  socket.on('set chat history', (msgs, id) => {
+    console.log(msgs);
+    console.log(id);
+
+    msgs.forEach(msg => {
+      const item = document.createElement('li');
+      item.textContent = msg.content;
+      if (msg.authorId == id) {
+        item.classList.add('repaly');
+      }
+      else {
+        item.classList.add('sender');
+      }
+      messages.appendChild(item);
+    });
+    window.scrollTo(0, document.body.scrollHeight);
+
+  });
+
+  // evento de enviar mensaje
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (input.value) {
+      socket.emit('new message', input.value, idArticle);
+      input.value = '';
+    }
+  });
+
+  // // evento de recibir mensaje
+  // socket.on('set message', (msg) => {
+  //   const item = document.createElement('li');
+  //   item.textContent = msg;
+  //   messages.appendChild(item);
+  //   window.scrollTo(0, document.body.scrollHeight);
+  // });
+
+  socket.on('set message', (msg, id) => {
+    console.log('set mensaje:', id);
+    console.log('set mensaje:', msg);
+    
+    const item = document.createElement('li');
+    item.textContent = msg.content;
+
+    // // Agrega la clase 'sender' al elemento 'li'
+    // item.classList.add('sender');
+
+    console.log('idSesion:', sesionId);
+    console.log('id:', id);
+    if (msg.authorId == sesionId) {
+      item.classList.add('repaly');
+    }
+    else if (msg.authorId == id) {
+      item.classList.add('sender');
+    }
+
+    messages.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight);
+  });
+
+
+
+
+
+  // evento de error
+  socket.on('socket error', (error) => {
+    //console.log('Error received from server: ', error.message);
+    console.error('An error occurred:', JSON.stringify(error, null, 2));
+    // Aquí puedes manejar el error, por ejemplo mostrando un mensaje de error al usuario
+  });
+
+});
---
+++ src/chat/eje1-custom/dashboard-simulate/dashboard.js
@@ -0,0 +1,37 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  //1//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJ1c3VhcmlvQGV4YW1wbGUuY29tIiwiaWF0IjoxNzE1Nzc5ODc3LCJleHAiOjE3MTU4NjYyNzd9.T9fU677tjYFISN_8a4cV04aXc56i3iHbprgweGPMfaA
+  //2//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1haWwiOiJ1c3VhcmlvQGhvdG1haWwuY29tIiwiaWF0IjoxNzE1Nzc5OTIyLCJleHAiOjE3MTU4NjYzMjJ9.HHf7gi3UEft4aKt62DSM86pH2vRDfYxEGSBsM_3R1QE
+
+  var tokenInput = document.getElementById('token');
+  // Agrega un manejador de eventos al campo de entrada
+  tokenInput.addEventListener('input', function () {
+    let token = String(tokenInput.value);
+    localStorage.setItem('token', token)
+    console.log('El valor del token es:', token);
+  });
+
+
+  var idInput = document.getElementById('postId');
+  // Agrega un manejador de eventos al campo de entrada
+  idInput.addEventListener('input', function () {
+    let postId = String(idInput.value);
+    console.log('El valor del id del post es:', postId);
+    localStorage.setItem('articleId', postId);
+  });
+
+
+
+  // var buttons = document.getElementsByName('article')
+  // // Agrega un manejador de eventos a cada botón
+  // buttons.forEach(function (button) {
+  //   button.addEventListener('click', function (event) {
+  //     // El botón que fue presionado es event.target
+  //     localStorage.setItem('idArticle', event.target.id);
+  //   });
+  // });
+
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+});
