f29ecd232ba7394b267ccbb3f8f8428d44857b5f
simonll4 <sllamosas806@alumnos.iua.edu.ar>
2024-05-25 13:39:08 -0300

    navBar: datos user y log out

--- src/page/specialist/dashboard/scripts/dashboard_specialist.js
+++ src/page/specialist/dashboard/scripts/dashboard_specialist.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
 
   const token = localStorage.getItem('token');
+  if (!token) {
+    window.location.href = "../../index.html";
+    return;
+  }
 
   //funcion para obtener el modelo de los articulos
   let existingArticle = [];
